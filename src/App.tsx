8dfc81f39ad886565750c7f70c99c0fea44d3bff
francomd <francomdalmasso@gmail.com>
2021-04-23 18:17:46 -0300

    Remove WebVitals and fix the styled main

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "twin.macro";
 import tw, { styled } from "twin.macro";
 
 const H1 = styled.h1(() => [tw`font-sans text-red-600 text-3xl font-bold`]);
-const Main = styled.main(() => [tw`text-`]);
+const Main = styled.main(() => [tw`text-red-800`]);
 
 function App() {
   return (
