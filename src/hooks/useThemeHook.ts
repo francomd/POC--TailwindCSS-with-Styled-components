bffdaeede8df3e63c0c0184f65b0d36d8a6c8108
francomd <francomdalmasso@gmail.com>
2021-04-26 17:38:54 -0300

    Fix the theme hook

--- src/hooks/useThemeHook.ts
+++ src/hooks/useThemeHook.ts
@@ -5,21 +5,25 @@ type Theme = "light" | "dark";
 export default (): [Theme, () => void] => {
   const isMountRef = useRef(true);
   const [theme, setTheme] = useState<Theme>("light");
-  const toggleTheme = () => setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
+  const toggleTheme = () =>
+    setTheme((prevTheme) => {
+      const newThemeState = prevTheme === "dark" ? "light" : "dark";
+      localStorage.theme = newThemeState;
+      return newThemeState;
+    });
 
   useEffect(() => {
     if (isMountRef.current) {
       isMountRef.current = false;
     } else {
       window.document.documentElement.classList.toggle("dark");
-      localStorage.theme = theme ? "dark" : "light";
     }
   }, [theme]);
 
   // Detect device theme conf
   useEffect(() => {
     if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) setTheme("dark");
-    if (localStorage.theme === "dark") setTheme("dark");
+    if (localStorage.theme) setTheme(localStorage.theme);
   }, []);
 
   return [theme, toggleTheme];
